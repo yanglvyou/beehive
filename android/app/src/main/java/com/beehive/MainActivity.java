5d87e4c764f3919fee5b263a57853781c4491366
lvyou <376607092@qq.com>
2021-07-24 18:25:47 +0800

    feat: 添加react-navigation 导航器

--- android/app/src/main/java/com/beehive/MainActivity.java
+++ android/app/src/main/java/com/beehive/MainActivity.java
@@ -1,8 +1,20 @@
 package com.beehive;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -12,4 +24,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "beehive";
   }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+     return new ReactActivityDelegate(this, getMainComponentName()) {
+       @Override
+       protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+       }
+     };
+   }
 }
