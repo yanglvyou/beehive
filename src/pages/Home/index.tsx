cafaf6fc474d1fbe8087fb40057aed6ccdb5c5b7
lvyou <376607092@qq.com>
2021-10-25 07:22:14 +0800

    chore: 完成应用图标相关配置

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { RootStackNavigation } from '@/navigator/index';
+import Config from 'react-native-config';
 
 interface IProps {
   navigation: RootStackNavigation;
 }
 
 const Home: React.FC<IProps> = ({ navigation }) => {
-  const goToDetail = () => {
-    navigation.navigate('Detail');
-  };
+  // const goToDetail = () => {
+  //   navigation.navigate('Detail');
+  // };
   return (
     <View>
-      <Text>Home</Text>
-      <Button
-        title="跳转到详情页"
-        onPress={() => {
-          goToDetail();
-        }}
-      />
+      <Text>URL:{Config.API_URL}</Text>
     </View>
   );
 };
